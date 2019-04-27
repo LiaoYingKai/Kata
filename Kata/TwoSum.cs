8f8b567b1f2fab8a561e3febc71b06001c42fac3
Kai Liao Ying-Kai <kai.liao28@titansoft.com.sg>
2019-04-27 14:52:27 +0800

    complete TwoSum and easy unit test

---
+++ Kata/TwoSum.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class TwoSum
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int[] addTwoSum(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            int[] answerList = new int[2];
+            int firstIndex = 0;
+            int lastIndex = nums.Length-1;
+            int sum = nums[firstIndex] + nums[lastIndex];
+            while (sum != target)
+            {
+                if (sum > target)
+                {
+                    lastIndex--;
+                }
+                else
+                {
+                    firstIndex++;
+                }
+                sum = nums[firstIndex] + nums[lastIndex];
+            }
+
+            answerList[0] = firstIndex;
+            answerList[1] = lastIndex;
+            
+            return answerList;
+        }
+    }
+}
---
+++ KataTest/TwoSumUnitTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kata;
+
+namespace KataTest
+{
+    [TestClass]
+    public class TwoSumUnitTest
+    {
+        public static void testing(int[] nums, int tergetSum , int[] answerNums)
+        {
+            int[] actual = TwoSum.addTwoSum(nums, tergetSum);
+            CollectionAssert.AreEqual(answerNums, actual);
+        }
+        [TestMethod]
+        public void input_2_7_11_15_target_is_9_shouldReturn_0_1()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var targetSum = 9;
+            var target = new int[] { 0, 1 };
+            testing(nums, targetSum, target);
+        }
+
+        [TestMethod]
+        public void input_2_7_11_15_target_is_18_shouldReturn_1_2()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var targetSum = 18;
+            var target = new int[] { 1, 2 };
+            testing(nums, targetSum, target);
+        }
+        [TestMethod]
+        public void input_1_2_7_11_15_18_target_is_19_shouldReturn_0_5()
+        {
+            var nums = new int[] { 1, 2, 7, 11, 15, 18};
+            var targetSum = 19;
+            var target = new int[] { 0, 5 };
+            testing(nums, targetSum, target);
+        }
+        
+    }
+}
